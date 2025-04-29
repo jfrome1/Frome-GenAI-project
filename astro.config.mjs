89df2b474808d582087d912ef8fdbdfe3eed72dd
unknown <joeyk2k@gmail.com>
2025-04-29 17:02:52 +0700

    disabled heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
         minHeadingLevel: 1,
         maxHeadingLevel: 2,
       },
+      markdown: {
+        headingLinks: false,
+      },
       plugins: [starlightNutshell()],
       title: "NUS GenAI Policy Project",
       components: {
